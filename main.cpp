31b98fe0f4a6b89369810a323b003132587347e7
maxvick <max.vickerson@gmail.com>
2018-02-08 23:21:48 +1000

    included basic timer code

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <util/twi.h>
 #include "Motor.h"
 #include "Robot.h"
 
@@ -13,6 +16,20 @@ int main(void)
 {
     //initialise the output port
 	DDRD = 0xFF; //enable all of port D as output pins
+
+	//Enable the Two Wire Interface module on PortE	
+	power_twi_enable();
+
+	//enable timer 0
+	power_timer0_enable();
+	TCCR0B |= ((1 << CS02) | (1 << CS00)); //scale by 1024
+	TCNT0 = 0x01; //set the counter to maximum time
+	TIMSK0 |= (1 << TOIE0); //enable the interrupt mask
+
+	//enable the global interrupt mask
+	sei();
+
+	
 	//have master enable pins as well - only enable once all init finished?
 
 	//initialise the Robot object & it's motors
@@ -29,20 +46,27 @@ int main(void)
 	myBot.setDirection(SLOW_RIGHT, 10);
 	myBot.setDirection(STOPPED, 10);
 
-	//initialise the TWI & interrupt vector
-
 	//initialise the timer & interrupt vector
 	
 	/* Replace with your application code */
+	int test = 0;
     while (1) 
     {
 		//listen for commands on I2C
-		
+		test++;
     }
 }
 
 void forwards(int duration) {
 
 
+}
+
+
+//Timer 0 overflow handler
+ISR(TIMER0_OVF_vect) {
+	int test = 5;
+	test += 66;
+	TIMSK0 &= ~(1 << TOIE0); // diable the mask
 }
 
